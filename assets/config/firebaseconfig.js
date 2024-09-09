fedd05fc9a1fecc09d59e6eda982597e5e7f383f
Mateus Pierre <mateus.mateus30@gmail.com>
2024-09-08 21:37:21 -0300

    Update firebaseconfig.js

--- assets/config/firebaseconfig.js
+++ assets/config/firebaseconfig.js
@@ -5,13 +5,13 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDl5NYPILCIaIy-i9IK9g6l1Gq5vYxEBGQ",
-  authDomain: "ambiental-112b3.firebaseapp.com",
-  projectId: "ambiental-112b3",
-  storageBucket: "ambiental-112b3.appspot.com",
-  messagingSenderId: "217491249145",
-  appId: "1:217491249145:web:c4a70e08c2e0591df44f28",
-  measurementId: "G-BRK8ZWGNLH"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
